a5961d0dd6e125d81a81a3696d787c4f2bd70260
Francis Cruz <jeni13franc@gmail.com>
2026-02-13 12:18:23 +1100

    feat: Rename project and app manifest names to include Jothipuram

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: 'Sagaya Madha Choir',
-        short_name: 'Choir App',
+        name: 'Sagaya Madha Choir Jothipuram',
+        short_name: 'Sagaya Madha Choir Jothipuram',
         description: 'Sagaya Madha Choir Song Book',
         theme_color: '#ffffff',
         icons: [
